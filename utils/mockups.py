aef2a72a9bef56dbae22b6c84dd40664d55a92d9
Daniel Simon <achipowa205@gmail.com>
2022-03-11 17:32:18 +0100

    refactor(data): restructurizing the mockup code

--- utils/mockups.py
+++ utils/mockups.py
@@ -16,6 +16,81 @@ def _mocked_loop(type):
     loop = unittest.mock.create_autospec(spec=type, set=True)
     return loop
 
+bot_data = {
+    'application_id': 123,
+    'guilds': unittest.mock.MagicMock(),
+    'intents': '',
+    'latency': 0.001,
+    'loop': _mocked_loop(asyncio.AbstractEventLoop),
+    'status': 'asd',
+    'user': 'asd',
+    'users': ['asd', 'asd2']
+}
+
+guild_data = {
+    'id': 1,
+    'name': 'guild',
+    'region': 'Europe',
+    'verification_level': 2,
+    'default_notications': 1,
+    'afk_timeout': 100,
+    'icon': "icon.png",
+    'banner': 'banner.png',
+    'mfa_level': 1,
+    'splash': 'splash.png',
+    'system_channel_id': 464033278631084042,
+    'description': 'hello world',
+    'max_presences': 10000,
+    'max_members': 100000,
+    'preferred_locale': 'UTC',
+    'owner_id': 1,
+    'afk_channel_id': 464033278631084042,
+}
+
+voice_channel_data = {
+    'bitrate': 192,
+    'created_at': datetime.now(),
+    'id': 2,
+    'name': 'General',
+    'position': 2,
+    'rtc_region': discord.VoiceRegion.amsterdam,
+    'type': discord.ChannelType.voice,
+    'user_limit': 10000,
+    'members': ['szia']
+}
+
+voice_state_data = {
+    'afk': False,
+    'deaf': False,
+    'mute': False,
+    'requested_to_speak_at': None, 
+    'self_deaf': False,
+    'self_mute': False,
+    'self_stream': False,
+    'self_video': False,
+    'suppress': False
+}
+
+member_data = {
+    'avatar': 'avatar.jpg',
+    'bot': False,
+    'id': 1,
+    'name': 'kapocsi',
+    'user': 'kapocsi',
+    'roles': []
+}
+
+channel_data = {
+    'id': 1,
+    'type': 'TextChannel',
+    'name': 'General',
+    'parent_id': 6969696969,
+    'topic': 'topic',
+    'position': 1,
+    'nsfw': False,
+    'last_message_id': 1,
+}
+
 class RedefinedMockMixin:
     child_mock_type = unittest.mock.MagicMock
     additional_spec_asyncs = None
@@ -44,16 +119,6 @@ class RedefinedMockMixin:
             raise AttributeError(mock_name)
         return klass(**kw)
 
-bot_data = {
-    'application_id': 123,
-    'guilds': unittest.mock.MagicMock(),
-    'intents': '',
-    'latency': 0.001,
-    'loop': _mocked_loop(asyncio.AbstractEventLoop),
-    'status': 'asd',
-    'user': 'asd',
-    'users': ['asd', 'asd2']
-}
 
 class MockBot(RedefinedMockMixin, unittest.mock.MagicMock):
     def __init__(self, **kw) -> None:
@@ -63,25 +128,6 @@ class MockBot(RedefinedMockMixin, unittest.mock.MagicMock):
     async def can_run(self, ctx):
         return True
 
-guild_data = {
-    'id': 1,
-    'name': 'guild',
-    'region': 'Europe',
-    'verification_level': 2,
-    'default_notications': 1,
-    'afk_timeout': 100,
-    'icon': "icon.png",
-    'banner': 'banner.png',
-    'mfa_level': 1,
-    'splash': 'splash.png',
-    'system_channel_id': 464033278631084042,
-    'description': 'hello world',
-    'max_presences': 10000,
-    'max_members': 100000,
-    'preferred_locale': 'UTC',
-    'owner_id': 1,
-    'afk_channel_id': 464033278631084042,
-}
 
 guild = discord.Guild(data=guild_data, state=unittest.mock.MagicMock())
 class MockGuild(RedefinedMockMixin, unittest.mock.MagicMock):
@@ -93,34 +139,12 @@ class MockGuild(RedefinedMockMixin, unittest.mock.MagicMock):
         self.me.guild_permissions.connect = kw.get('connect', True)
         self.me.guild_permissions.speak = kw.get('speak', True)
 
-voice_channel_data = {
-    'bitrate': 192,
-    'created_at': datetime.now(),
-    'id': 2,
-    'name': 'General',
-    'position': 2,
-    'rtc_region': discord.VoiceRegion.amsterdam,
-    'type': discord.ChannelType.voice,
-    'user_limit': 10000,
-    'members': ['szia']
-}
 
 class MockVoiceChannel(RedefinedMockMixin, unittest.mock.MagicMock):
     default = voice_channel_data
     def __init__(self, **kw) -> None:
         super().__init__(**collections.ChainMap(self.default,kw))
 
-voice_state_data = {
-    'afk': False,
-    'deaf': False,
-    'mute': False,
-    'requested_to_speak_at': None, 
-    'self_deaf': False,
-    'self_mute': False,
-    'self_stream': False,
-    'self_video': False,
-    'suppress': False
-}
 
 class MockVoiceState(RedefinedMockMixin, unittest.mock.MagicMock):
     default = voice_state_data
@@ -128,14 +152,6 @@ class MockVoiceState(RedefinedMockMixin, unittest.mock.MagicMock):
         super().__init__(**collections.ChainMap(self.default,kw))
         self.channel = kw.get('channel', MockVoiceChannel())
 
-member_data = {
-    'avatar': 'avatar.jpg',
-    'bot': False,
-    'id': 1,
-    'name': 'kapocsi',
-    'user': 'kapocsi',
-    'roles': []
-}
 
 class MockMember(RedefinedMockMixin, unittest.mock.MagicMock):
     default = member_data
@@ -143,16 +159,7 @@ class MockMember(RedefinedMockMixin, unittest.mock.MagicMock):
         super().__init__(**collections.ChainMap(self.default,kw))
         self.voice = kw.get('voice', MockVoiceState())
 
-channel_data = {
-    'id': 1,
-    'type': 'TextChannel',
-    'name': 'General',
-    'parent_id': 6969696969,
-    'topic': 'topic',
-    'position': 1,
-    'nsfw': False,
-    'last_message_id': 1,
-}
+
 channel = discord.TextChannel(state=unittest.mock.MagicMock(), guild=MockGuild(), data=channel_data)
 
 class MockTextChannel(RedefinedMockMixin, unittest.mock.MagicMock):
