d9089961e51d307c9fd3ca01c204de503e3e9191
Daniel Simon <achipowa205@gmail.com>
2022-03-17 12:47:37 +0100

    refactor(tests): adding test setups for test cases

--- tests/test/test_connect.py
+++ tests/test/test_connect.py
@@ -5,15 +5,17 @@ from essentials.musicbot import MusicBot
 from essentials.commands.network.connect import ConnectCommand
 
 class TestConnectCommand(unittest.TestCase):
+    def setUp(self):
+        self.bot = MockBot()
+        self.music_bot = MusicBot(bot)
+    
     def test_connection_successful(self):
-        bot = MockBot()
-        music_bot = MusicBot(bot)
         ctx = MockContext()
-        connect_cmd = ConnectCommand(ctx,music_bot)
+        connect_cmd = ConnectCommand(ctx,self.music_bot)
         asyncio.run(connect_cmd.run(logging=False))
         voice_channel_id = ctx.author.voice.channel.id
         guild_id = ctx.guild.id
         text_channel_id = ctx.channel.id
-        server = music_bot.get_server(guild_id)
+        server = self.music_bot.get_server(guild_id)
         self.assertEqual(voice_channel_id, server['voice_channel'].id)
         self.assertEqual(text_channel_id, server['text_channel'].id)
--- tests/test/test_disconnect.py
+++ tests/test/test_disconnect.py
@@ -5,13 +5,15 @@ from essentials.musicbot import MusicBot
 from essentials.commands.network.disconnect import DisconnectCommand
 
 class TestDisconnectCommand(unittest.TestCase):
+    def setUp(self):
+        self.bot = MockBot()
+        self.music_bot = MusicBot(bot)
+    
     def test_disconnection_successful(self):
-        bot = MockBot()
-        music_bot = MusicBot(bot)
         ctx = MockContext()
-        music_bot.servers[ctx.guild.id] = {'voice_channel': ctx.voice.channel, 'text_channel': ctx.channel}
-        disconnect_cmd = DisconnectCommand(ctx,music_bot)
+        self.music_bot.servers[ctx.guild.id] = {'voice_channel': ctx.voice.channel, 'text_channel': ctx.channel}
+        disconnect_cmd = DisconnectCommand(ctx,self.music_bot)
         asyncio.run(disconnect_cmd.run(logging=False))
         guild_id = ctx.guild.id
-        server = music_bot.get_server(guild_id)
+        server = self.music_bot.get_server(guild_id)
         self.assertFalse(server)
