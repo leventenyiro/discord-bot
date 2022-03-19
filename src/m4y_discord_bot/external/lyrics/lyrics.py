cf6f8332b4d785932569238d689a388867218df8
Levente Nyírő <nyiro.levente@gmail.com>
2022-03-19 12:41:00 +0100

    lyrics.py végleges kialakítása

--- src/m4y_discord_bot/external/lyrics/lyrics.py
+++ src/m4y_discord_bot/external/lyrics/lyrics.py
@@ -9,6 +9,5 @@ class Lyrics:
         botinfo = json.loads(file.read())
         
         genius = lyricsgenius.Genius(botinfo["genius_access_token"])
-        lyricsgenius.PublicAPI()
         lyrics = re.sub(r'[0-9]*Embed$', '', genius.search_song(song_title).lyrics)
         return lyrics
