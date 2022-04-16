2634ed5fb6a7ead51ec0ecd9527badda7b219032
Nyírő Levente Gyula <b4kzan@inf.elte.hu>
2022-04-16 08:36:53 +0200

    fix(remove): add (index > 1) error and test case

--- tests/test/test_remove.py
+++ tests/test/test_remove.py
@@ -11,7 +11,8 @@ class TestRemove(unittest.TestCase):
         'no_song': 'There is no song currently playing!',
         'not_the_same_text_channel': 'Commands can be accessed from {0}!',
         'not_the_same_voice_channel': 'We are not in the same room!',
-        'index_out_of_range': 'Playlist index is out of range!'
+        'index_out_of_range': 'Playlist index is out of range!',
+        'skip_instead_of_remove': 'You should use skip command instead of this!'
     }
 
     def setUp(self):
@@ -31,10 +32,11 @@ class TestRemove(unittest.TestCase):
         }
         self.music_bot.add_server(ctx.guild.id, server)
         audio_player.add_to_playlist(unittest.mock.MagicMock())
-        self.assertEqual(audio_player.get_playlist_length(), 1)
-        command = RemoveCommand(ctx, self.music_bot, 1)
+        audio_player.add_to_playlist(unittest.mock.MagicMock())
+        self.assertEqual(audio_player.get_playlist_length(), 2)
+        command = RemoveCommand(ctx, self.music_bot, 2)
         result = asyncio.run(command.run(logging=False))
-        self.assertEqual(audio_player.get_playlist_length(), 0)
+        self.assertEqual(audio_player.get_playlist_length(), 1)
         self.assertIsNone(result)
 
     def test_remove_no_server(self):
@@ -107,3 +109,20 @@ class TestRemove(unittest.TestCase):
         command = RemoveCommand(ctx, self.music_bot, 2)
         result = asyncio.run(command.run(logging=False))
         self.assertEqual(result, self.error_messages['index_out_of_range'])
+
+    def test_remove_skip_instead_of_remove(self):
+        ctx = MockContext()
+        voice_channel = ctx.author.voice.channel
+        text_channel = ctx.channel
+        mock_voice_client = MockVoiceClient()
+        audio_player = AudioPlayer(mock_voice_client)
+        server = {
+            'voice_channel': voice_channel, 
+            'text_channel': text_channel,
+            'audio_player': audio_player
+        }
+        self.music_bot.add_server(ctx.guild.id, server)
+        audio_player.add_to_playlist(unittest.mock.MagicMock())
+        command = RemoveCommand(ctx, self.music_bot, 1)
+        result = asyncio.run(command.run(logging=False))
+        self.assertEqual(result, self.error_messages['skip_instead_of_remove'])
