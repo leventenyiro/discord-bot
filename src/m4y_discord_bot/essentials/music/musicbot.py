788f5c99dbc8bd08d86ae20aba3846b7a72c97ca
Daniel Simon <achipowa205@gmail.com>
2022-04-17 13:54:24 +0200

    feat(commands): added daycore command

--- src/m4y_discord_bot/essentials/music/musicbot.py
+++ src/m4y_discord_bot/essentials/music/musicbot.py
@@ -7,6 +7,7 @@ from essentials.commands.network.disconnect import DisconnectCommand
 from essentials.commands.music.skip import SkipCommand
 from essentials.commands.music.remove import RemoveCommand
 from essentials.commands.music.nightcore import NightcoreCommand
+from essentials.commands.music.daycore import DaycoreCommand
 from essentials.music.song import Song
 
 class MusicBot:
@@ -50,6 +51,10 @@ class MusicBot:
 
     async def nightcore(self, ctx):
         cmd = NightcoreCommand(ctx, self)
+        await cmd.run()
+
+    async def daycore(self, ctx):
+        cmd = DaycoreCommand(ctx, self)
         await cmd.run()
 
     def add_server(self, id, server):
