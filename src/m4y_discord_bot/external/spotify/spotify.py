aa714e716c692a9f15cce83059b790c59e711ca4
Nyírő Levente Gyula <b4kzan@inf.elte.hu>
2022-05-01 09:46:16 +0200

    feat(test_spotify_api): final implementation

--- src/m4y_discord_bot/external/spotify/spotify.py
+++ src/m4y_discord_bot/external/spotify/spotify.py
@@ -5,17 +5,22 @@ from base64 import b64encode
 
 class Spotify:
     @staticmethod
-    def get_track(url):
-        file = open("./botconfig.json", "r")
-        botinfo = json.loads(file.read())
-        user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
-        response_auth = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
+    def get_track(url, **kw):
+        spotify_object = kw.get('spotify', requests)
+        try:
+            file = open("./botconfig.json", "r")
+            botinfo = json.loads(file.read())
+            user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
+        except:
+            user_pass = None
+            pass
+
+        response_auth = spotify_object.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
 
         track_id = urlparse(url).path.split('/track/')[1]
         endpoint = f'https://api.spotify.com/v1/tracks/{track_id}'
         headers = {'Authorization': f'Bearer {response_auth.json()["access_token"]}'}
-        response = requests.get(endpoint, headers=headers)
-
+        response = spotify_object.get(endpoint, headers=headers)
         track = response.json()
         try:
             return track["artists"][0]["name"] + " - " + track["name"]
@@ -23,16 +28,22 @@ class Spotify:
             raise Exception("Invalid link")
 
     @staticmethod
-    def get_playlist(url):
-        file = open("./botconfig.json", "r")
-        botinfo = json.loads(file.read())
-        user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
-        response_auth = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
+    def get_playlist(url, **kw):
+        spotify_object = kw.get('spotify', requests)
+        try:
+            file = open("./botconfig.json", "r")
+            botinfo = json.loads(file.read())
+            user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
+        except:
+            user_pass = None
+            pass
 
+        response_auth = spotify_object.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
+        
         playlist_id = urlparse(url).path.split('/playlist/')[1]
         endpoint = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(artists, name))'
         headers = {'Authorization': f'Bearer {response_auth.json()["access_token"]}'}
-        response = requests.get(endpoint, headers=headers)
+        response = spotify_object.get(endpoint, headers=headers)
 
         playlist = response.json()
         tracks = []
@@ -44,16 +55,22 @@ class Spotify:
             raise Exception("Invalid link")
 
     @staticmethod
-    def get_album(url):
-        file = open("./botconfig.json", "r")
-        botinfo = json.loads(file.read())
-        user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
-        response_auth = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
+    def get_album(url, **kw):
+        spotify_object = kw.get('spotify', requests)
+        try:
+            file = open("./botconfig.json", "r")
+            botinfo = json.loads(file.read())
+            user_pass = b64encode(bytes(f'{botinfo["spotify_client_id"]}:{botinfo["spotify_client_secret"]}', encoding='utf8')).decode("ascii")
+        except:
+            user_pass = None
+            pass
+
+        response_auth = spotify_object.post('https://accounts.spotify.com/api/token', headers={'Authorization': f'Basic {user_pass}'}, data={'grant_type': 'client_credentials'})
 
         album_id = urlparse(url).path.split('/album/')[1]
         endpoint = f'https://api.spotify.com/v1/albums/{album_id}/tracks'
         headers = {'Authorization': f'Bearer {response_auth.json()["access_token"]}'}
-        response = requests.get(endpoint, headers=headers)
+        response = spotify_object.get(endpoint, headers=headers)
 
         album = response.json()
         tracks = []
--- tests/test/test_spotify_api.py
+++ tests/test/test_spotify_api.py
@@ -1,15 +1,27 @@
 import unittest
-from urllib import response
+
 from external.spotify.spotify import Spotify
 from utility.mockups import *
 
 class TestSpotifyApi(unittest.TestCase):
     def setUp(self):
-        self.url = None
         self.spotifyapi = unittest.mock.MagicMock()
         title = unittest.mock.MagicMock()
-        self.spotifyapi.get_track.return_value = title
+        self.request = unittest.mock.MagicMock()
+        self.request.post.return_value = title
+        self.kwargs = { 'spotify': self.spotifyapi }
 
-    def test_spotify_api_basic(self):
-        Spotify.get_track(self.url)
-        self.spotifyapi.assert_called()
+    def test_spotify_api_get_track(self):
+        Spotify.get_track("/track/", **self.kwargs)
+        self.spotifyapi.post.assert_called()
+        self.spotifyapi.get.assert_called()
+
+    def test_spotify_api_get_playlist(self):
+        Spotify.get_playlist("/playlist/", **self.kwargs)
+        self.spotifyapi.post.assert_called()
+        self.spotifyapi.get.assert_called()
+
+    def test_spotify_api_get_album(self):
+        Spotify.get_album("/album/", **self.kwargs)
+        self.spotifyapi.post.assert_called()
+        self.spotifyapi.get.assert_called()
